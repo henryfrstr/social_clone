21db73bb2709b03611e0245ae24336ed0c7121bc
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2020-12-16 17:13:40 +0300

    added auth

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accounts'
 ]
 
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
+LOGIN_URL = "accounts:login"
+LOGIN_REDIRECT_URL = 'home'
